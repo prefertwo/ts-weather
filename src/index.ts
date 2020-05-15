60b4b9f0c6918851d788ee9dac220d3c8f6be825
prefertwo <1522876658@qq.com>
2020-05-15 13:25:07 +0800

    add commander

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-const name:string = 'guo';
-
-export default name;
+// const {program} = require('commander')
+import colors from 'colors'
+import commander from 'commander'
+//  from 'commander'
+commander
+  .version("5.1.0")
+  .option("-p, --peppers", "Add peppers")
+  .option("-P, --pineapple", "Add pineapple")
+  .option("-b, --bbq-sauce", "Add bbq sauce")
+  .option("-c, --cheese [type]", "Add the specified type of cheese [marble]", "marble")
+  .option("-c, --city [name]", "Add city name")
+  .parse(process.argv);
+if( !commander.city) {
+  commander.outputHelp(colors.red);
+} else {
+  // tslint:disable-next-line: no-console
+  console.log(commander.city);
+  process.exit();
+}
